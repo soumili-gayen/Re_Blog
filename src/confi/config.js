631a5b0869daf7a984cb05e8d0d1348a8a8b18df
soumili-gayen <soumiligayen48@gmail.com>
2025-08-01 21:04:58 +0530

    Auth Primary

--- src/confi/config.js
+++ src/confi/config.js
@@ -1,9 +1,9 @@
 const conf = {
   appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
-  appwriteProject_Id: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-  appwriteDatabase_Id: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-  appwriteCollection_Id: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-  appwriteBucket_Id: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+  appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+  appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+  appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+  appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
 }
 
 export default conf
