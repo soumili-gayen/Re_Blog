e7f26e51406aed3e9073179ca753b4ce49322329
soumili-gayen <soumiligayen48@gmail.com>
2025-08-07 14:28:23 +0530

    all commit

--- src/appWrite/auth.js
+++ src/appWrite/auth.js
@@ -1,5 +1,6 @@
-import conf from '../config.js';
+import conf from '../conf/conf.js';
 import { Client, Account, ID } from "appwrite";
+
 
 export class AuthService {
     client = new Client();
@@ -55,8 +56,5 @@ export class AuthService {
     }
 }
 
-const authService = new AuthService();
-
+const authService = new AuthService()
 export default authService
-
-
--- src/appWrite/config.js
+++ src/appWrite/config.js
@@ -1,5 +1,6 @@
-import conf from '../config.js';
+import conf from '../conf/conf.js';
 import { Client, ID, Databases, Storage, Query } from "appwrite";
+
 export class Service{
     client = new Client();
     databases;
